6abe1b634d3f083b1321cb02a3d79e073b6e1e04
hermitli <hermitli@tencent.com>
2021-07-06 21:31:00 +0800

    Sample Elimination WIP

--- Source/RenderPasses/SampleEliminatePass/SampleEliminatePass.cpp
+++ Source/RenderPasses/SampleEliminatePass/SampleEliminatePass.cpp
@@ -25,18 +25,13 @@
  # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **************************************************************************/
-#include "VirtualLightVisPass.h"
-#include "Utils/VirtualLight/VirtualLightContainer.h"
+#include "SampleEliminatePass.h"
+
 
 namespace
 {
     const char kDummy[] = "dummy";
     const char kDesc[] = "Insert pass description here";
-    const char kRadius[] = "radius";
-    
-    const char kPosChannel[] = "pos";
-    const char kOutputChannel[] = "output";
-
     const std::string kDicInitialVirtualLights = "initialVirtualLights";
     const std::string kDicCurVirtualLights = "curVirtualLights";
 }
@@ -47,85 +42,60 @@ extern "C" __declspec(dllexport) const char* getProjDir()
     return PROJECT_DIR;
 }
 
-extern "C" __declspec(dllexport) void getPasses(Falcor::RenderPassLibrary& lib)
+static void regPythonApi(pybind11::module& m)
 {
-    lib.registerClass("VirtualLightVisPass", kDesc, VirtualLightVisPass::create);
+    pybind11::class_<SampleEliminatePass, RenderPass, SampleEliminatePass::SharedPtr> pass(m, "SampleEliminatePass");
 }
 
-VirtualLightVisPass::SharedPtr VirtualLightVisPass::create(RenderContext* pRenderContext, const Dictionary& dict)
+extern "C" __declspec(dllexport) void getPasses(Falcor::RenderPassLibrary& lib)
 {
-    SharedPtr pPass = SharedPtr(new VirtualLightVisPass);
+    lib.registerClass("SampleEliminatePass", kDesc, SampleEliminatePass::create);
+    ScriptBindings::registerBinding(regPythonApi);
+}
+
+SampleEliminatePass::SharedPtr SampleEliminatePass::create(RenderContext* pRenderContext, const Dictionary& dict)
+{
+    SharedPtr pPass = SharedPtr(new SampleEliminatePass);
     for (const auto& [key, value] : dict)
     {
-        if (key == kRadius)
-        {
-            pPass->mRadius = value;
-        }
     }
     pPass->mpSampleGenerator = SampleGenerator::create(SAMPLE_GENERATOR_UNIFORM);
-
-    Program::Desc desc;
-    desc.addShaderLibrary("RenderPasses/VirtualLightVisPass/VirtualLightVis.cs.slang").csEntry("main").setShaderModel("6_5");
-    pPass->mpComputePass = ComputePass::create(desc, Program::DefineList(), false);
-
     return pPass;
 }
 
-std::string VirtualLightVisPass::getDesc()
+std::string SampleEliminatePass::getDesc()
 {
     return kDesc;
 }
 
-Dictionary VirtualLightVisPass::getScriptingDictionary()
+Dictionary SampleEliminatePass::getScriptingDictionary()
 {
     Dictionary d;
-    d[kRadius] = mRadius;
     return d;
 }
 
-RenderPassReflection VirtualLightVisPass::reflect(const CompileData& compileData)
+RenderPassReflection SampleEliminatePass::reflect(const CompileData& compileData)
 {
     // Define the required resources here
     RenderPassReflection reflector;
-    reflector.addInput(kDummy, "");
-    reflector.addInput(kPosChannel, "pos texture").bindFlags(Falcor::ResourceBindFlags::UnorderedAccess | Falcor::ResourceBindFlags::ShaderResource);
-    reflector.addOutput(kOutputChannel, "output texture").bindFlags(ResourceBindFlags::RenderTarget | ResourceBindFlags::UnorderedAccess | ResourceBindFlags::ShaderResource).format(ResourceFormat::RGBA32Float);
     return reflector;
 }
 
-void VirtualLightVisPass::execute(RenderContext* pRenderContext, const RenderData& renderData)
+void SampleEliminatePass::execute(RenderContext* pRenderContext, const RenderData& renderData)
 {
+    // renderData holds the requested resources
+    // auto& pTexture = renderData["src"]->asTexture();
     if (mpScene == nullptr)
     {
         return;
     }
-
-    Texture::SharedPtr pPos = renderData[kPosChannel]->asTexture();
-    Texture::SharedPtr pDst = renderData[kOutputChannel]->asTexture();
-    VirtualLightContainer::SharedPtr curVirtualLights = renderData.getDictionary()[kDicCurVirtualLights];
-
-    if (curVirtualLights == nullptr)
-    {
-        debugBreak(); // should not be nullptr here
-        return;
-    }
-
-    ShaderVar cb = mpComputePass["CB"];
-    cb["gViewportDims"] = uint2(pDst->getWidth(), pDst->getHeight());
-    cb["gFrameIndex"] = gpFramework->getGlobalClock().getFrame();
-    cb["gRadius"] = mRadius;
-    curVirtualLights->setShaderData(cb["gVirtualLightContainer"]);
-    mpComputePass["gPos"] = pPos;
-    mpComputePass["gOutput"] = pDst;
-    mpComputePass->execute(pRenderContext, uint3(pDst->getWidth(), pDst->getHeight(), 1));
 }
 
-void VirtualLightVisPass::renderUI(Gui::Widgets& widget)
+void SampleEliminatePass::renderUI(Gui::Widgets& widget)
 {
-    widget.var("Vis Radius", mRadius, 0.0f, 1.0f);
 }
 
-void VirtualLightVisPass::setScene(RenderContext* pRenderContext, const Scene::SharedPtr& pScene)
+void SampleEliminatePass::setScene(RenderContext* pRenderContext, const Scene::SharedPtr& pScene)
 {
     mpScene = pScene;
 
@@ -135,8 +105,7 @@ void VirtualLightVisPass::setScene(RenderContext* pRenderContext, const Scene::S
         defines.add(mpSampleGenerator->getDefines());
         defines.add("_MS_DISABLE_ALPHA_TEST");
         defines.add("_DEFAULT_ALPHA_TEST");
-
-        mpComputePass->getProgram()->addDefines(defines);
-        mpComputePass->setVars(nullptr); // Trigger recompile
     }
 }
+
+
