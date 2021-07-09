1405526c109b5c34fa7e11112abfdf24231f17ac
hermitli <hermitli@tencent.com>
2021-07-09 22:55:07 +0800

    Virtual Light Estimate WIP

---
+++ Source/RenderPasses/VirtualLightEstimatePass/VirtualLightEstimatePass.cpp
@@ -0,0 +1,114 @@
+/***************************************************************************
+ # Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions
+ # are met:
+ #  * Redistributions of source code must retain the above copyright
+ #    notice, this list of conditions and the following disclaimer.
+ #  * Redistributions in binary form must reproduce the above copyright
+ #    notice, this list of conditions and the following disclaimer in the
+ #    documentation and/or other materials provided with the distribution.
+ #  * Neither the name of NVIDIA CORPORATION nor the names of its
+ #    contributors may be used to endorse or promote products derived
+ #    from this software without specific prior written permission.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ # PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **************************************************************************/
+#include "VirtualLightEstimatePass.h"
+
+
+namespace
+{
+    const char kDummy[] = "dummy";
+    const char kDummyInput[] = "input";
+    const char kDummyOutput[] = "output";
+    const char kDesc[] = "Insert pass description here";
+    const std::string kDicInitialVirtualLights = "initialVirtualLights";
+    const std::string kDicSampleEliminatedVirtualLights = "sampleEliminatedVirtualLights";
+    const std::string kDicCurVirtualLights = "curVirtualLights";
+}
+
+// Don't remove this. it's required for hot-reload to function properly
+extern "C" __declspec(dllexport) const char* getProjDir()
+{
+    return PROJECT_DIR;
+}
+
+static void regPythonApi(pybind11::module& m)
+{
+    pybind11::class_<VirtualLightEstimatePass, RenderPass, VirtualLightEstimatePass::SharedPtr> pass(m, "VirtualLightEstimatePass");
+}
+
+extern "C" __declspec(dllexport) void getPasses(Falcor::RenderPassLibrary& lib)
+{
+    lib.registerClass("VirtualLightEstimatePass", kDesc, VirtualLightEstimatePass::create);
+    ScriptBindings::registerBinding(regPythonApi);
+}
+
+VirtualLightEstimatePass::SharedPtr VirtualLightEstimatePass::create(RenderContext* pRenderContext, const Dictionary& dict)
+{
+    SharedPtr pPass = SharedPtr(new VirtualLightEstimatePass);
+    for (const auto& [key, value] : dict)
+    {
+    }
+    pPass->mpSampleGenerator = SampleGenerator::create(SAMPLE_GENERATOR_UNIFORM);
+    return pPass;
+}
+
+std::string VirtualLightEstimatePass::getDesc()
+{
+    return kDesc;
+}
+
+Dictionary VirtualLightEstimatePass::getScriptingDictionary()
+{
+    Dictionary d;
+    return d;
+}
+
+RenderPassReflection VirtualLightEstimatePass::reflect(const CompileData& compileData)
+{
+    // Define the required resources here
+    RenderPassReflection reflector;
+    return reflector;
+}
+
+void VirtualLightEstimatePass::execute(RenderContext* pRenderContext, const RenderData& renderData)
+{
+    // renderData holds the requested resources
+    // auto& pTexture = renderData["src"]->asTexture();
+    if (mpScene == nullptr)
+    {
+        return;
+    }
+}
+
+void VirtualLightEstimatePass::renderUI(Gui::Widgets& widget)
+{
+}
+
+void VirtualLightEstimatePass::setScene(RenderContext* pRenderContext, const Scene::SharedPtr& pScene)
+{
+    mpScene = pScene;
+
+    if (mpScene)
+    {
+        Shader::DefineList defines = mpScene->getSceneDefines();
+        defines.add(mpSampleGenerator->getDefines());
+        defines.add("_MS_DISABLE_ALPHA_TEST");
+        defines.add("_DEFAULT_ALPHA_TEST");
+    }
+}
+
+
